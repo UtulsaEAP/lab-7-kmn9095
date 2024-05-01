3ae686341af153b2fd9cf3a11f5261c9ede095cc
Kelsey N <kmn9095@utulsa.edu>
2024-04-30 22:50:00 -0500

    lab7.imtiredofthisgrandpa

--- Problem 1/exceptionHandling.py
+++ Problem 1/exceptionHandling.py
@@ -1,16 +1,21 @@
-def exceptionHandling():
-    # Split input into 2 parts: name and age
+while True:
+    # Read input and split it into parts
     parts = input().split()
     name = parts[0]
-    while name != '-1':
-        # FIXME: The following line will throw ValueError exception.
-        #        Insert try/except blocks to catch the exception.
+
+    # Check if name is -1 to exit the loop
+    if name == '-1':
+        break
+    #try-block 
+    try:
+        # Try to convert the second element to an integer and add 1 to it
         age = int(parts[1]) + 1
         print(f'{name} {age}')
-        
-        # Get next line
-        parts = input().split()
-        name = parts[0]
+    #except block to catch the exception when converting string to integer 
+    except ValueError:
+        # If the conversion fails, set age to 0 and print the name and age
+        age = 0
+        print(f'{name} {age}')
 
 if __name__ == '__main__':
     exceptionHandling()
--- Problem 2/wordInRange.py
+++ Problem 2/wordInRange.py
@@ -1,5 +1,22 @@
 def wordInRange():
-    #Type your code here
-    return
+    # Read input file name and search range from the user
+    file_name = input()
+    lower_bound = input()
+    upper_bound = input()
+
+    # Read file contents
+    with open(file_name, 'r') as file:
+        lines = file.readlines()
+
+    # Iterate over each line and check if it falls within the range
+    for line in lines:
+        word = line.strip()
+        if lower_bound <= word <= upper_bound:
+            print(word + " - in range")
+        else:
+            print(word + " - not in range")
+
+    # Print a newline to end the output
+    print()
 if __name__ == '__main__':
     wordInRange()
